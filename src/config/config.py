1ac8bde3b63971ad4be7659ebf6cb8b01fc68702
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-04-18 09:23:11 +0700

    add config file

---
+++ src/config/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+DATA_DIR = Path(__file__).parent.parent.parent / "data"
+IMG_DIR = Path(__file__).parent.parent.parent / "img"
+MAPS_DIR = Path(__file__).parent.parent.parent / "maps"
